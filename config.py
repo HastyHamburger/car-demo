277127b52d0743f2683bb684aad6b46e40f17c03
Alex Walker <kny6trpv@gmail.com>
2026-04-29 15:15:22 -0500

    Added simple wall collision resolution

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ class Config:
     screen_width = 1280
     screen_height = 720
     throttle_speed = .3
-    steering_speed = .025
+    steering_speed = .008
     steering_exponent = 1.2
-    steering_centering = .3
+    steering_centering = .9
     drag = .95
     car_height = 40
     car_width = 80
